d8dcea6fb7caa061e77c6936645a9a2d87bb4f38
Gabriel Terwesten <gabriel@terwesten.net>
2022-05-20 10:06:02 +0200

    chore: upgrade Nx deps

--- packages/add-nx-dart-to-monorepo/jest.config.ts
+++ packages/add-nx-dart-to-monorepo/jest.config.ts
@@ -1,6 +1,6 @@
-module.exports = {
+export default {
   displayName: 'add-nx-dart-to-monorepo',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
